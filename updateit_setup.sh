c6ad10365bcc9020d682c0782ac835d301821824
w` <sissypr0hyx@gmail.com>
2026-02-01 18:53:09 +0100

    Update updateit_setup.sh

--- updateit_setup.sh
+++ updateit_setup.sh
@@ -2,11 +2,11 @@
 UPDATEIT="updateit.py"
 UPDATEIT_ALIAS="updateit"
 chmod +x "$UPDATEIT"
-sudo cp "$UPDATEIT" "/usr/local/bin/$UPDATEIT_ALIAS"
+cp "$UPDATEIT" "$HOME/.local/bin/$UPDATEIT_ALIAS"
 mkdir -p "$HOME/.local/share/updateit"
 LATEST_LOGGER="$HOME/.local/share/updateit/latest.log"
 if [ ! -f "$LATEST_LOGGER" ]; then
     touch "$LATEST_LOGGER"
 fi
 
-echo "Updateit v2 installed"
+echo "Full updateit installed"

1076e1f40e6bf372cb76608c8980b89d80e260de
w` <sissypr0hyx@gmail.com>
2026-02-01 18:40:06 +0100

    Update updateit.py

--- updateit.py
+++ updateit.py
@@ -63,7 +63,7 @@ def refresh():
     resp = r.get(url)
     if resp.status_code == 200:
         with open("updateit.py", "wb") as f:
-            f.write(r.content)
+            f.write(resp.content)
         print("Succesfully fetch update")
     else:
         print("Failed to fetch update")
